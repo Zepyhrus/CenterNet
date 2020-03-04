5431889dd9609b406a83bc7bd0947ac635314347
Zepyhrus <sherkfung@gmail.com>
2020-03-04 17:34:34 +0800

    Compatible with pytorch==1.4

--- src/test.py
+++ src/test.py
@@ -13,6 +13,7 @@ from progress.bar import Bar
 import torch
 
 from external.nms import soft_nms
+# from lib.external import soft_nms
 from opts import opts
 from logger import Logger
 from utils.utils import AverageMeter
